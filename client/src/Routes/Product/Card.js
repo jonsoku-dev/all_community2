c50a1d043e2985da09d0a41f67b24494094b089c
rakutenjongseok <whdtjr2792@gmail.com>
2019-10-16 16:24:04 +0900

    refactoring

---
+++ client/src/Routes/Product/Card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+  background-color: #3498db;
+  > div {
+    word-break: break-all;
+  }
+`;
+const Photo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  img {
+    width: 100px;
+    height: 100px;
+  }
+`;
+const Name = styled.div``;
+const Description = styled.div``;
+const Price = styled.div``;
+
+const Card = ({ product: { _id, name, description, price, photo } }) => {
+  return (
+    <CardWrapper>
+      <Photo>
+        <img src={`http://localhost:4000/${photo}`} alt="" />
+      </Photo>
+      <Name>{name}</Name>
+      <Description>{description.substring(0, 40) + '...'}</Description>
+      <Price>${price}</Price>
+    </CardWrapper>
+  );
+};
+
+export default Card;
---
+++ client/src/Routes/index.js
@@ -0,0 +1,17 @@
+import Main from './Main/index';
+import Signup from './Signup/index';
+import Login from './Login/index';
+import Post from './Post/index';
+import { CategoryCreateContainer, CategoryPutContainer } from './Category';
+import { ProductCreateContainer, ProductMainContainer } from './Product';
+
+export {
+  Main,
+  Signup,
+  Login,
+  Post,
+  CategoryCreateContainer,
+  CategoryPutContainer,
+  ProductCreateContainer,
+  ProductMainContainer,
+};
